52902d932248e2af539af4fdaab96f5e17f109a6
yassine <yassinealouini@outlook.com>
2015-11-23 08:18:59 +0100

    Now everything works: bundling, watch and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
 
 var paths = {
   scripts: ['index.js']
 };
 
 gulp.task('watch', function() {
-  plugins.livereload.listen();
-  gulp.watch(paths.scripts, ['scripts']);
+  livereload.listen();
+  gulp.watch(paths.scripts, ['browserify']);
 });
 
 gulp.task('browserify', function() {
@@ -19,10 +21,17 @@ gulp.task('browserify', function() {
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('bundle.js'))
         // Start piping stream to tasks!
-        .pipe(gulp.dest('./'));
-        .pipe(plugins.livereload());
-})
+        .pipe(gulp.dest('./'))
+        .pipe(livereload())
+        .pipe(connect.reload());
+});
 
 
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
 
-gulp.task('default', ['watch']);
+
+gulp.task('default', ['watch', 'connect', 'browserify']);
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 
-var d3 = require('./node_modules/d3/d3.min.js');
+var d3 = require('d3');
 
 //Test if d3.js works
 console.log(d3);
 let myArray = d3.range(1000).map(x => 2 * x);
+var paragraphs = d3.select("body").selectAll("p").style("color", "red");
